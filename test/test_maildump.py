82270e60cc9c2f522909d95ec26bac27ed81a03d
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-04-16 06:48:09 -0400

    Moved tests in test directory, test_send_mail

--- test/test_maildump.py
+++ test/test_maildump.py
@@ -2,6 +2,7 @@ import subprocess
 import unittest
 import time
 import requests
+import smtplib
 
 
 class TestMailDump(unittest.TestCase):
@@ -17,17 +18,29 @@ class TestMailDump(unittest.TestCase):
 
 
 class TestSendMail(TestMailDump):
-    url_ = 'http://127.0.0.1:1080'
+    host = '127.0.0.1'
+    http_port = '1080'
+    smtp_port = '1025'  # TODO put in TestMailDump
+    url = 'http://' + host + ':' + http_port
 
     def test_get_index(self):
-        res = requests.get(self.url_)
+        res = requests.get(self.url)
         self.assertEquals(200, res.status_code)
 
     def test_get_messages_empty(self):
-        res = requests.get('http://127.0.0.1:1080/messages/')
+        res = requests.get(self.url + '/messages/')
         self.assertEquals(200, res.status_code)
         json = res.json()
         self.assertEquals(0, len(json.get('messages')))
+
+    def test_send_mail(self):
+        mail = smtplib.SMTP(self.host, self.smtp_port)
+        msg = 'test'
+        mail.sendmail('sender@test.com', 'recipient@test.com', msg)
+        res = requests.get(self.url + '/messages/')
+        self.assertEquals(200, res.status_code)
+        json = res.json()
+        self.assertEquals(1, len(json.get('messages')))
 
 if __name__ == '__main__':
     unittest.main()
